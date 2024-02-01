2747938189799df1b7dbf02c7b2cc7e2c7bc11d7
enikotyc <55724724+enikotyc@users.noreply.github.com>
2024-02-01 12:24:30 -0500

    editing index

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 
 const router = createRouter({
-  base: '/app',
+
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
